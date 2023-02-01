efdec2a94052445cada250550d44098a90070f39
Кияница Денис <kiyanitsadenis@gmail.com>
2023-02-01 23:07:02 +0700

    add customer interface, structure, init function, methods for start and close

--- internal/app/consumer/db.go
+++ internal/app/consumer/db.go
@@ -1,5 +1,12 @@
 package consumer
 
+import (
+	"github.com/Kiatsyndesi/api_tg_bot/internal/app/repo"
+	"github.com/Kiatsyndesi/api_tg_bot/internal/model"
+	"sync"
+	"time"
+)
+
 /*Первоочередная цель консьюмера - получить данные из БД
 У консьюмера есть 2 метода - открыть и закрыть соединение самого себя с БД
 
@@ -27,3 +34,79 @@ package consumer
 1. Ждем выполнения всех горутин
 2. закрываем канал с булевыми
 */
+
+type ICustomer interface {
+	Start()
+	Close()
+}
+
+type Customer struct {
+	n         uint64
+	events    chan<- model.PhonesEvent
+	repo      repo.EventRepo
+	butchSize uint64
+	timeout   time.Duration
+	doneChan  chan bool
+	wg        *sync.WaitGroup
+}
+
+type Config struct {
+	n         uint64
+	events    chan<- model.PhonesEvent
+	repo      repo.EventRepo
+	butchSize uint64
+	timeout   time.Duration
+}
+
+func NewCustomer(
+	numberOfCustomers uint64,
+	eventsWriting chan<- model.PhonesEvent,
+	repo repo.EventRepo,
+	butchSize uint64,
+	timeout time.Duration) *Customer {
+
+	wg := &sync.WaitGroup{}
+	doneChan := make(chan bool)
+	return &Customer{
+		n:         numberOfCustomers,
+		events:    eventsWriting,
+		repo:      repo,
+		butchSize: butchSize,
+		timeout:   timeout,
+		doneChan:  doneChan,
+		wg:        wg,
+	}
+}
+
+func (c *Customer) Start() {
+	for i := uint64(0); i < c.n; i++ {
+		c.wg.Add(1)
+
+		go func() {
+			defer c.wg.Done()
+			ticker := time.NewTicker(c.timeout)
+
+			for {
+				select {
+				case <-ticker.C:
+					events, err := c.repo.Lock(c.butchSize)
+
+					if err != nil {
+						continue
+					}
+
+					for _, event := range events {
+						c.events <- event
+					}
+				case <-c.doneChan:
+					return
+				}
+			}
+		}()
+	}
+}
+
+func (c *Customer) Close() {
+	c.wg.Wait()
+	close(c.doneChan)
+}
