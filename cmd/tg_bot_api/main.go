47b215691d07e66993dbb3887646f12f056f83df
Кияница Денис <kiyanitsadenis@gmail.com>
2023-02-05 22:27:11 +0700

    add main

--- cmd/tg_bot_api/main.go
+++ cmd/tg_bot_api/main.go
@@ -1,1 +1,28 @@
-package tg_bot_api
+package main
+
+import (
+	"github.com/Kiatsyndesi/api_tg_bot/internal/app/retranslator"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+
+	cfg := retranslator.Config{
+		ChannelSize:     512,
+		ConsumerCount:   2,
+		ConsumerTimeout: 1,
+		ConsumerSize:    10,
+		ProducerCount:   28,
+		WorkerCount:     2,
+	}
+
+	retranslator := retranslator.NewRetranslator(cfg)
+	retranslator.Start()
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigs
+}
